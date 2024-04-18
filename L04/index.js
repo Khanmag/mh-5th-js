1e68d65a23ae6efb1902ddbe0fd99012ce7d8748
Your Name <mukulay96@gma.com>
2024-04-18 20:10:38 +0300

    k4

--- L04/index.js
+++ L04/index.js
@@ -16,30 +16,50 @@ const obj = {
 // console.log( obj.num );
 // console.log( obj["num"] );
 
-const userData = {
-  name: "Andrey",
-  login: "ANDY123",
-  jobs: {
-    doctor: "5 years",
-  },
-  // метод (объекта) - функция внутри объекта
-  //
-  // changeName(newName) {
-  //   userData.name = newName
-  // }
-  //
-  // changeName: function(newName) {
-  //   userData.name = newName
-  // }
-  //
-  changeName: (newName) => {
-    userData.name = newName;
+// const userData = {
+//   name: "Andrey",
+//   login: "ANDY123",
+//   jobs: {
+//     doctor: "5 years",
+//   },
+// метод (объекта) - функция внутри объекта
+//
+// changeName(newName) {
+//   userData.name = newName
+// }
+//
+// changeName: function(newName) {
+//   userData.name = newName
+// }
+//
+//   changeName: (newName) => {
+//     userData.name = newName;
+//   },
+// };
+
+// const user2 = {};
+
+// const user1 = userData;
+// user1.login = "USER1";
+
+// for (key in userData) {
+//   console.log(key);
+//   user2[key] = userData[key];
+// }
+
+// console.log("userData", userData);
+// // userData.changeName("Artyom");
+// console.log("user1", user1);
+
+// user2.login = "Sasha";
+// console.log("userData", userData);
+// console.log("user2", user2);
+
+const player = {
+  nick: "KILLER",
+  points: 0,
+  startGame() {
+    const userValue = confirm("ОРЕЛ-ДА, РЕШКА-НЕТ");
+    const yesOrNo = Math;
   },
 };
-
-const user1 = userData;
-user1.login = "USER1";
-
-console.log("userData", userData);
-// userData.changeName("Artyom");
-console.log("user1", user1);
