d91e858073a07682713d44dfee2c48871612c668
Your Name <mukulay96@gma.com>
2024-04-18 19:34:48 +0300

    l4

---
+++ L03/index.js
@@ -0,0 +1,10 @@
+// let wantToContinue = confirm("Положить сахар?");
+
+// do {
+//   wantToContinue = confirm("положить еще?");
+// } while (wantToContinue);
+
+for (let i = 0; ; i++) {
+  if (i < 1000) console.log(i);
+  else break;
+}
