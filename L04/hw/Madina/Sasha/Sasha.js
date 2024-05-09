cd70eeb68936a40280ac78417860b7f75b559ad0
Your Name <mukulay96@gma.com>
2024-05-09 19:47:22 +0300

    l1

---
+++ L04/hw/Madina/Sasha/Sasha.js
@@ -0,0 +1,20 @@
+const player = {
+  nick: "CUBE",
+  points: 0,
+  startGame() {
+    const userValue = confirm ("Играем?");
+    const yesOrNo = Math.random() > 0.0;
+    if (userValue == yesOrNo) {
+      alert("Делайте один шаг вперед");
+      else if 
+    //   if ((yesOrNo = Math.random() > 0.166)) {
+    //     alert("Делайте два шага вперед");
+    //   }
+    // }
+    // Math.random() > 0.166 ||
+    //   Math.random() > 0.332 ||
+    //   Math.random() > 0.498 ||
+    //   Math.random() > 0.664 ||
+    //   Math.random() > 0.83;
+  },
+};
