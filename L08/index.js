914b4976a3effa2eebc6f5e977f4a8a5f4c5a02a
Shamil <khanmag329@gmail.com>
2024-05-12 12:06:34 +0300

    L08

--- L08/index.js
+++ L08/index.js
@@ -1,21 +1,20 @@
-
-const myName = "Olesya"
+const myName = "Olesya";
 
 const simpleFunc = () => {
   console.log(`${myName}'s FUNC!`);
-} 
+};
 // simpleFunc()
 
 const countCreator = (num) => {
-  let count = 0
+  let count = 0;
   return () => {
-    console.log(`counter${num}`, count++);
-    // return 
-  }
-}
+    console.log(`counter${num} ${count++}`);
+    // return
+  };
+};
 // const counter1 = countCreator
 // console.log("counter1", counter1);
-const counter2 = countCreator(2)
+const counter2 = countCreator(2);
 // counter2 - функция, которую вернул countCreator
 //() => {
 //  console.log(count++);
@@ -27,20 +26,59 @@ const counter2 = countCreator(2)
 // 0 - от консоли в counter2
 // counter2 - 2  undefined - от консоли выше
 
-counter2() // 0
+// counter2(); // 0
 // undefined
-counter2() // 1
-counter2() // 2
-counter2() // 3
+// counter2(); // 1
+// counter2(); // 2
+// counter2(); // 3
 
-const counter3 = countCreator(3)
-counter3()
+const counter3 = countCreator(3);
+// counter3();
 //
+const callsCounter = (name) => {
+  let count = 0;
+  return () => console.log(`${name}'s calls - ${++count}`);
+};
 
-
-
+const annasCalls = callsCounter("Anna");
+const billsCalls = callsCounter("Bill");
 // annasCalls() // 1
 // billsCalls() // 1
 // billsCalls() // 2
 // billsCalls() // 3
+
+// console.log(window);
+// console.log(this);
+// console.log(global);
+// console.log(globalThis);
+
+
+// const powFunc = (a, n) => {
+//   return a ** n
+// }
+const powFunc = (a, n) => {
+  console.log(`a = ${a}, n = ${n}`);
+  return (n === 1) ? a : a * powFunc(a, n - 1)
+}
+
+const timer = (secs) => {
+  console.log(secs);
+  return (secs === 0) ? 0 : timer(secs - 1)
+} 
+
+// let i = 0
+// const numCounter = (count) => {
+//   console.log(i++);
+//   return (count === 30) ? 30 : timer(count + 1)
+// }
+
+// const result = powFunc(3, 5)
+// 3, 5
+// 3 * (3, 4)
+// 3 * 3 * (3, 3)
+// 3 * 3 * 3 *(3, 2)
+// 3 * 3 * 3 * 3 * 3
+// console.log(result);
+
+timer(20)
 
